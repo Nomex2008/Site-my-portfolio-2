aa4443f704791a1e39591bec7db23889094c9362
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-07-23 21:11:37 +0300

    Fix bags
    Fix bags.

--- js/mainJS.js
+++ js/mainJS.js
@@ -1,21 +1,6 @@
 const BtnDarkMode = document.querySelector('#BtnDarkMode')
 const body = document.querySelector('#body')
 
-//sistem night
-if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
-    BtnDarkMode.classList.add('dark-mode-btn-active');
-    body.classList.add('dark');
-}
-
-//localstorage check night
-if(localStorage.getItem('darkMode') === 'dark'){
-    BtnDarkMode.classList.add('dark-mode-btn-active');
-    body.classList.add('dark');
-} else if(localStorage.getItem('darkMode') === 'light'){
-    BtnDarkMode.classList.remove('dark-mode-btn-acrtive');
-    body.classList.remove('dark');
-}
-
 //add night class after click
 BtnDarkMode.onclick = function(){
     BtnDarkMode.classList.toggle('dark-mode-btn-active');
@@ -26,4 +11,13 @@ BtnDarkMode.onclick = function(){
     } else{
         localStorage.setItem('darkMode','light')
     }
+}
+
+//localstorage check night
+if(localStorage.getItem('darkMode') === 'dark'){
+    BtnDarkMode.classList.add('dark-mode-btn-active');
+    body.classList.add('dark');
+} else if(localStorage.getItem('darkMode') === 'light'){
+    BtnDarkMode.classList.remove('dark-mode-btn-acrtive');
+    body.classList.remove('dark');
 }
